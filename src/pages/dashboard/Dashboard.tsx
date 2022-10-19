253cd9bb1a4779904cfefb771a072fd9c1437ba4
EDUARDO BUBA <eduardobuba21@gmail.com>
2022-10-18 23:10:40 -0300

    feat: routes and theme

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,22 @@
+import { Text, View } from 'react-native';
+
+import { useTheme } from 'styled-components';
+
+// ----------------------------------------------------------------------
+
+export default function Dashboard() {
+  const theme = useTheme();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.palette.background.default,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text>Meu App!</Text>
+    </View>
+  );
+}
