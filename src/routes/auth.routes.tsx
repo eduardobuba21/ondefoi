119146e3812ebe067e1e740610c724fb45e34c9a
EDUARDO BUBA <eduardobuba21@gmail.com>
2022-11-26 20:58:47 -0300

    fix: status bar color on splah and auth screens

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -3,6 +3,9 @@ import {
   createStackNavigator,
   CardStyleInterpolators,
 } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+// hooks
+import { useTheme } from '@src/hooks/useTheme';
 // screens
 import { SignIn } from '@src/screens/auth/sign-in/SignIn';
 import { SignUp } from '@src/screens/auth/sign-up/SignUp';
@@ -20,17 +23,22 @@ export type SignUpScreenProps = StackScreenProps<RootParamList, 'SignUp'>;
 // ----------------------------------------------------------------------
 
 export function AuthRoutes() {
+  const theme = useTheme();
   const Stack = createStackNavigator<RootParamList>();
 
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-      }}
-    >
-      <Stack.Screen name="SignIn" component={SignIn} />
-      <Stack.Screen name="SignUp" component={SignUp} />
-    </Stack.Navigator>
+    <>
+      <StatusBar style="light" backgroundColor={theme.palette.background.default} />
+
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        }}
+      >
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </>
   );
 }
