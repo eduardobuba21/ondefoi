7d2e2bf7a3d8de29af4e33540afc97b0dc56cb10
EDUARDO BUBA <eduardobuba21@gmail.com>
2022-11-26 23:22:34 -0300

    fix: auth transitions

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -3,13 +3,12 @@ import * as NavigationBar from 'expo-navigation-bar';
 import { StackScreenProps, createStackNavigator } from '@react-navigation/stack';
 // hooks
 import { useTheme } from '@src/hooks/useTheme';
+// components
+import { Animated } from 'react-native';
+import { addTransition } from '@src/components/animations/ScreenTransition';
 // screens
 import { SignIn } from '@src/screens/auth/sign-in/SignIn';
 import { SignUp } from '@src/screens/auth/sign-up/SignUp';
-
-//
-import { Animated } from 'react-native';
-import { addTransition } from '@src/components/animations/ScreenTransition';
 
 // ----------------------------------------------------------------------
 
@@ -36,33 +35,33 @@ export function AuthRoutes() {
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
-          animationEnabled: false,
-          // cardStyleInterpolator: ({ current, next }) => {
-          //   const progress = Animated.add(current.progress, next ? next.progress : 0);
+          // animationEnabled: false,
+          cardStyleInterpolator: ({ current, next }) => {
+            const progress = Animated.add(current.progress, next ? next.progress : 0);
 
-          //   return {
-          //     cardStyle: {
-          //       opacity: progress.interpolate({
-          //         inputRange: [0, 0.5, 1, 1.5, 2],
-          //         outputRange: [0, 0, 1, 0, 0],
-          //       }),
-          //     },
-          //   };
-          // },
-          // transitionSpec: {
-          //   open: {
-          //     animation: 'timing',
-          //     config: {
-          //       duration: 250,
-          //     },
-          //   },
-          //   close: {
-          //     animation: 'timing',
-          //     config: {
-          //       duration: 250,
-          //     },
-          //   },
-          // },
+            return {
+              cardStyle: {
+                opacity: progress.interpolate({
+                  inputRange: [0, 1, 2],
+                  outputRange: [0, 1, 0],
+                }),
+              },
+            };
+          },
+          transitionSpec: {
+            open: {
+              animation: 'timing',
+              config: {
+                duration: 250,
+              },
+            },
+            close: {
+              animation: 'timing',
+              config: {
+                duration: 250,
+              },
+            },
+          },
         }}
       >
         <Stack.Screen name="SignIn" component={addTransition(SignIn)} />
